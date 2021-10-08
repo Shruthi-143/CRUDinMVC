af6a7843b48c40c80be2ac302f0010120272209e
pavan <pavan@com>
2021-10-08 13:55:34 +0530

    adding name

--- CRUDinMVC/Models/StudentModel.cs
+++ CRUDinMVC/Models/StudentModel.cs
@@ -10,10 +10,10 @@ namespace CRUDinMVC.Models
         [Required(ErrorMessage = "First name is required.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "City is required.")]
+        [Required(ErrorMessage = "City Name is required.")]
         public string City { get; set; }
 
-        [Required(ErrorMessage = "Address is required.")]
+        [Required(ErrorMessage = "Address is as  required.")]
         public string Address { get; set; }
     }
 }
